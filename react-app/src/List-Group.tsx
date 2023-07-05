20816708bdc983acdbd507eb774171cbc90291c7
Andrew Falbo <falbokev@gmail.com>
2023-07-05 10:53:37 -0400

    edit

--- react-app/src/List-Group.tsx
+++ react-app/src/List-Group.tsx
@@ -10,9 +10,7 @@ const ListGroup = () => {
         <div className="card" style={{ width: "18rem" }}>
           <img src="..." className="card-img-top" alt="..." />
           <div className="card-body">
-            <h5 className="card-title">
-              {animalList.map((animalList) => animalList.animal)}
-            </h5>
+            <h5 className="card-title">Card Title</h5>
             <p className="card-text">
               Some quick example text to build on the card title and make up the
               bulk of the card's content.
