0bbe538d7f941102dc7d50a25359c770ea3d78ea
Andrew Falbo <falbokev@gmail.com>
2023-07-05 15:29:18 -0400

    Icons

--- react-app/src/List-Group.tsx
+++ react-app/src/List-Group.tsx
@@ -17,15 +17,31 @@ const ListGroup = () => {
           >
             <img
               src={animalList.image}
-              className="card-img-top img-fluid"
+              className="card-img-top"
               alt={animalList.animal}
             />
             <div className="card-body">
-              <h5 className="card-title">{animalList.animal}</h5>
-              <p className="card-text">Class: {animalList.class}</p>
-              <a href="#" className="btn btn-primary">
-                Go somewhere
-              </a>
+              <div className="row">
+                <div className="col">
+                  <h5 className="card-title">{animalList.animal}</h5>
+                </div>
+                <div className="col"></div>
+                <div className="col">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="red"
+                    className="bi bi-heart"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
+                  </svg>
+                </div>
+              </div>
+              <div className="col">
+                <p className="card-text">Class: {animalList.class}</p>
+              </div>
             </div>
           </div>
         ))}
