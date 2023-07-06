b1cb8c7cbeae33eacb74e493e0c1eb2defe0cde7
Andrew Falbo <falbokev@gmail.com>
2023-07-05 22:18:04 -0400

    navbar creation

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Counter from "./Counter/Counter";
 import AnimalList from "./Animal-List/Animal-List";
+import Navbar from "./Navbar";
 
 const App = () => {
   return (
     <div>
-      <Counter heading={"Counter"} />
+      <Navbar />
       <AnimalList />
     </div>
   );
