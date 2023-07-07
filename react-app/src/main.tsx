fa14d69ed5507505e6970a2923b76d70b3a63f54
Andrew Falbo <falbokev@gmail.com>
2023-07-07 09:36:55 -0400

    sizing problem

--- react-app/src/main.tsx
+++ react-app/src/main.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import $ from "jquery";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
