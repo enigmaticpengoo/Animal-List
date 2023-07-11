e8d7eb75e847c8ad5ca2cb8b21a7de3e6fd31059
Andrew Falbo <falbokev@gmail.com>
2023-07-11 13:27:56 -0400

    filter appearence

--- react-app/src/Filter.tsx
+++ react-app/src/Filter.tsx
@@ -1,31 +1,63 @@
 import { useState } from "react";
 
 const Filter = () => {
-  const [filter, setFilter] = useState("null");
+  const [filterOne, setFilterOne] = useState("null");
+  const [filterTwo, setFilterTwo] = useState("null");
 
-  const filterSelect = () => {
+  const filterSelectOne = () => {
     const selectedFilter = (
-      document.getElementById("filter") as HTMLInputElement
+      document.getElementById("filterOne") as HTMLInputElement
     ).value;
-    setFilter(selectedFilter);
+    setFilterOne(selectedFilter);
     console.log(selectedFilter);
+
+    filterApply();
+  };
+
+  const filterSelectTwo = () => {
+    const selectedFilter = (
+      document.getElementById("filterTwo") as HTMLInputElement
+    ).value;
+    setFilterOne(selectedFilter);
+    console.log(selectedFilter);
+
     filterApply();
   };
 
   const filterApply = () => {
-    console.log(filter);
+    console.log(filterOne);
   };
 
   return (
-    <div style={{ margin: "10px 80px" }}>
-      <label htmlFor="filter" style={{ marginRight: "5px" }}>
-        Filter:
-      </label>
-      <select onChange={filterSelect} name="filter" id="filter">
-        <option value=""></option>
-        <option value="Ascending">Ascending</option>
-        <option value="Descending">Descending</option>
-      </select>
+    <div style={{ display: "flex", flexWrap: "nowrap" }}>
+      <div style={{ margin: "10px 10px 10px 80px" }}>
+        <label htmlFor="filterOne" style={{ marginRight: "5px" }}>
+          Filters:
+        </label>
+        <select
+          onChange={filterSelectOne}
+          name="filterOne"
+          id="filterOne"
+          style={{ width: "110px" }}
+        >
+          <option value=""></option>
+          <option value="Ascending">Ascending</option>
+          <option value="Descending">Descending</option>
+        </select>
+      </div>
+      <div style={{ margin: "10px 0px" }}>
+        <select
+          onChange={filterSelectTwo}
+          name="filterTwo"
+          id="filterTwo"
+          style={{ width: "110px" }}
+        >
+          <option value=""></option>
+          <option value="Name">Name</option>
+          <option value="Class">Class</option>
+          <option value="Genus">Genus</option>
+        </select>
+      </div>
     </div>
   );
 };
