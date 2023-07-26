b09e0b8244b14dc1db07dd837647353fce81ef0f
Andrew Falbo <falbokev@gmail.com>
2023-07-26 11:58:32 -0400

    link sends to pexel

--- react-app/src/Animal-List/Animal-List-Data.ts
+++ react-app/src/Animal-List/Animal-List-Data.ts
@@ -5,23 +5,23 @@ const animalList: {
   image: string;
   link: string;
 }[] = [
-    { animal: "Dog", clas: "Mammal", genus:"Canis", image: "src/assets/dog.jpg", link:"https://www.en.wikipedia.org/wiki/Dog"  },
-    { animal: "Cat", clas: "Mammal", genus:"Felis", image: "src/assets/cat.jpg", link:"https://www.en.wikipedia.org/wiki/Cat"  },
-    { animal: "Mouse", clas: "Mammal", genus:"Mus", image: "src/assets/mouse.jpg", link:"https://www.en.wikipedia.org/wiki/Mouse"  },
-    { animal: "Penguin", clas: "Bird", genus:"Pinguinus", image: "src/assets/penguin.jpg", link:"https://www.en.wikipedia.org/wiki/Penguin"  },
-    { animal: "Bear", clas: "Mammal", genus:"Ursus", image: "src/assets/bear.jpg", link:"https://www.en.wikipedia.org/wiki/Bear"  },
-    { animal: "Wolf", clas: "Mammal", genus:"Canis", image: "src/assets/wolf.jpg", link:"https://www.en.wikipedia.org/wiki/Wolf"  },
-    { animal: "Owl", clas: "Bird", genus:"Strix", image: "src/assets/owl.jpg", link:"https://www.en.wikipedia.org/wiki/Owl"  },
-    { animal: "Tiger", clas: "Mammal", genus:"Panthera", image: "src/assets/tiger.jpg", link:"https://www.en.wikipedia.org/wiki/Tiger"  },
-    { animal: "Deer", clas: "Mammal", genus:"Cervidae", image: "src/assets/deer.jpg", link:"https://www.en.wikipedia.org/wiki/Deer" },
-    { animal: "Rabbit", clas: "Mammal", genus:"Leporidae", image: "src/assets/rabbit.jpg", link:"https://www.en.wikipedia.org/wiki/Rabbit"  },
-    { animal: "Lion", clas: "Mammal", genus:"Panthera", image: "src/assets/lion.jpg", link:"https://www.en.wikipedia.org/wiki/Lion"  },
-    { animal: "Whale", clas: "Mammal", genus:"Cetartiodactyla", image: "src/assets/whale.jpg", link:"https://www.en.wikipedia.org/wiki/Whale"  },
-    { animal: "Hawk", clas: "Bird", genus:"Buteo", image: "src/assets/hawk.jpg", link:"https://www.en.wikipedia.org/wiki/Hawk"  },
-    { animal: "Duck", clas: "Bird", genus:"Anseriformes", image: "src/assets/duck.jpg", link:"https://www.en.wikipedia.org/wiki/Duck"  },
-    { animal: "Eagle", clas: "Bird", genus:"Aquila", image: "src/assets/eagle.jpg", link:"https://www.en.wikipedia.org/wiki/Eagle"  },
-    { animal: "Coyote", clas: "Mammal", genus:"Canis", image: "src/assets/coyote.jpg", link:"https://www.en.wikipedia.org/wiki/Coyote"  },
-    { animal: "Vulture", clas: "Bird", genus:"Cathartes", image: "src/assets/vulture.jpg", link:"https://www.en.wikipedia.org/wiki/Vulture"  }
+    { animal: "Dog", clas: "Mammal", genus:"Canis", image: "src/assets/dog.jpg", link:"https://www.pexels.com/search/Dog"  },
+    { animal: "Cat", clas: "Mammal", genus:"Felis", image: "src/assets/cat.jpg", link:"https://www.pexels.com/search/Cat"  },
+    { animal: "Mouse", clas: "Mammal", genus:"Mus", image: "src/assets/mouse.jpg", link:"https://www.pexels.com/search/Mouse"  },
+    { animal: "Penguin", clas: "Bird", genus:"Pinguinus", image: "src/assets/penguin.jpg", link:"https://www.pexels.com/search/Penguin"  },
+    { animal: "Bear", clas: "Mammal", genus:"Ursus", image: "src/assets/bear.jpg", link:"https://www.pexels.com/search/Bear"  },
+    { animal: "Wolf", clas: "Mammal", genus:"Canis", image: "src/assets/wolf.jpg", link:"https://www.pexels.com/search/Wolf"  },
+    { animal: "Owl", clas: "Bird", genus:"Strix", image: "src/assets/owl.jpg", link:"https://www.pexels.com/search/Owl"  },
+    { animal: "Tiger", clas: "Mammal", genus:"Panthera", image: "src/assets/tiger.jpg", link:"https://www.pexels.com/search/Tiger"  },
+    { animal: "Deer", clas: "Mammal", genus:"Cervidae", image: "src/assets/deer.jpg", link:"https://www.pexels.com/search/Deer" },
+    { animal: "Rabbit", clas: "Mammal", genus:"Leporidae", image: "src/assets/rabbit.jpg", link:"https://www.pexels.com/search/Rabbit"  },
+    { animal: "Lion", clas: "Mammal", genus:"Panthera", image: "src/assets/lion.jpg", link:"https://www.pexels.com/search/Lion"  },
+    { animal: "Whale", clas: "Mammal", genus:"Cetartiodactyla", image: "src/assets/whale.jpg", link:"https://www.pexels.com/search/Whale"  },
+    { animal: "Hawk", clas: "Bird", genus:"Buteo", image: "src/assets/hawk.jpg", link:"https://www.pexels.com/search/Hawk"  },
+    { animal: "Duck", clas: "Bird", genus:"Anseriformes", image: "src/assets/duck.jpg", link:"https://www.pexels.com/search/Duck"  },
+    { animal: "Eagle", clas: "Bird", genus:"Aquila", image: "src/assets/eagle.jpg", link:"https://www.pexels.com/search/Eagle"  },
+    { animal: "Coyote", clas: "Mammal", genus:"Canis", image: "src/assets/coyote.jpg", link:"https://www.pexels.com/search/Coyote"  },
+    { animal: "Vulture", clas: "Bird", genus:"Cathartes", image: "src/assets/vulture.jpg", link:"https://www.pexels.com/search/Vulture"  }
   ];
 
   export default animalList
